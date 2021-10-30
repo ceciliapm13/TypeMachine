2ee80c391f86476b790c90f30230e6c36f858262
Cecília Machado <cecilia.pmachado@protonmail.com>
2021-10-30 21:31:44 +0000

    Start Game & Player Implementation

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,76 @@
+import org.academiadecodigo.bootcamp.Prompt;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Game {
 
-    public Game(){
+    private Player player;
+    private Prompt prompt;
+    private Timer timer;
+
+    public Game() {
+        Prompt prompt = new Prompt(System.in, System.out);
+        Timer timer = new Timer();
+    }
+
+    //game init: boas-vindas ao jogo
+    public void init() {
+        //Timer
 
     }
 
-    public void teste(){
 
+    //game start: início do jogo a partir do momento em que dois players estão conectados
+    public void start() {
+        /*while(lista de expressões não tiver acabado) {
+            nextChallenge();
+            awaitPlayerInput();
+        }
+        */
+    }
+
+
+
+    //
+    public void nextChallenge() {
+        //Timer
+
+    }
+
+    //
+    public void awaitPlayerInput() {
+        //Timer
+        //if(timer = !isOver && playerInput.equals()arrayWord)
+
+    }
+
+    public void endMessage() {
+
+    }
+
+    public void timeExpired(/*int number*/) {
+        /*
+        for ( i = number; i <= number; i--){
+            terminalwriter (i);
+            timer.schedule(new ExpireTask(callbackClass), 1000);
+         */ // este sout só se aplica
+    }
+
+
+
+    //---------------------- EXPIRED TASK --------------------------------------
+    public class ExpireTask extends TimerTask {
+
+        private Game game;
+
+        ExpireTask(Game game) {
+            this.game = game;
+        }
+
+        public void run() {
+            game.timeExpired();
+        }
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,33 @@
+import java.net.Socket;
+
 public class Player {
+
+    private Socket userSocket;
+    private String name;
+    // private ... color;
+    private int score;
+
+    public Player(Socket userSocket) {
+        this.userSocket = userSocket;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String setName(String name) {
+        return this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int score(int score) {
+        return this.score = score;
+    }
+
+
+
 }
